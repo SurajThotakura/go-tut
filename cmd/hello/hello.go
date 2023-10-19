81e352f05ac2fe420ab0e2960dadaf4c0df21f1e
Suraj Thotakura <31972379+SurajThotakura@users.noreply.github.com>
2023-10-19 23:38:36 +0530

    feat: add more languages refactor

--- cmd/hello/hello.go
+++ cmd/hello/hello.go
@@ -4,15 +4,44 @@ import (
 	"fmt"
 )
 
-func SayHello(name string) string {
+const (
+	english          = "English"
+	ENGLISH_GREETING = "Hello, "
+
+	telugu          = "Telugu"
+	TELUGU_GREETING = "Namaskaram, "
+
+	hindi          = "Hindi"
+	HINDI_GREETING = "Namaste, "
+)
+
+func GetGreeting(language string) string {
+
+	const ENGLISH_GREETING = "Hello, "
+	const TELUGU_GREETING = "Namaskaram, "
+	const HINDI_GREETING = "Namaste, "
+
+	switch language {
+	case english:
+		return ENGLISH_GREETING
+	case telugu:
+		return TELUGU_GREETING
+	case hindi:
+		return HINDI_GREETING
+	default:
+		return ENGLISH_GREETING
+	}
+}
+
+func SayHello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	const greeting = "Hello, "
+	greeting := GetGreeting(language)
 	greetUserString := greeting + name
 	return greetUserString
 }
 
 func main() {
-	fmt.Println(SayHello("Suraj"))
+	fmt.Println(SayHello("Suraj", "English"))
 }
