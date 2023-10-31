dff47606ec61f5108d3feed52b7f4684617387ed
Suraj Thotakura <31972379+SurajThotakura@users.noreply.github.com>
2023-10-31 21:42:32 +0530

    feat: add select lesson

---
+++ cmd/select/select.go
@@ -0,0 +1,22 @@
+package selectLesson
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) string {
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
+
+	if aDuration < bDuration {
+		return a
+	}
+	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	startTime := time.Now()
+	http.Get(url)
+	return time.Since(startTime)
+}
